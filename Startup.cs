4f61c7de1a09ce3d29adeb4bddf288b1d1a28498
Hiep Nguyen Thanh <thanhhiep77777@gmail.com>
2021-12-13 10:24:12 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HMTStationery.Startup))]
+namespace HMTStationery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
